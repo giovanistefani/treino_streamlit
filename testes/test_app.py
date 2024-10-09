163a7eb21a9ea2c51e7e6b69257d889a8d4313ff
Giovani Stefani <gstefani@infointelligence.com.br>
2024-10-09 08:40:26 -0300

    test: adicionando teste de aplicação

---
+++ testes/test_app.py
@@ -0,0 +1,26 @@
+import subprocess
+
+import pytest 
+from selenium import webdrive
+from time import sleep
+
+@pytest.fixture
+def driver():
+    processo = subprocess.Popen(['streamlit', 'run', 'main.py'])
+    driver = webdriver.Chrome()
+    driver.get('http://localhost:8501')
+    driver.set_page_load_timeout(10)
+
+    yield driver
+
+    driver.quit()
+    driver.kill()
+
+def test_open_app(driver):
+    driver.get('http://localhost:8501')
+    sleep(2)
+
+def test_title_app(driver):
+    driver.get('http://localhost:8501')
+    sleep(2)
+    assert driver.title == 'Validador de CSV'    
